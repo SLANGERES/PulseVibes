dbd3d9e8dd608deaef17fa0dd9843677b2acd0a2
SLANGERES <karannautiyal22@gmail.com>
2025-01-20 18:53:13 +0530

    Frontend Changes

--- Frontend/frontendfile/src/App.jsx
+++ Frontend/frontendfile/src/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './components/Header';
 import { Section } from './components/Section';
 import { Service } from './components/Service';
 import './App.css'; // Import your CSS
+import { Loading } from './components/Loading';
+import { Footer } from './components/Footer';
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <>
-      <Header/>
-      <Section/>
-      <Service/>
-    </>
+    <div className="App">
+      <Header />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+
+          <Section />
+        </>
+      )}
+      <Footer/>
+    </div>
+    
   );
 }
 
--- Frontend/frontendfile/src/components/Service.jsx
+++ Frontend/frontendfile/src/components/Service.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { forwardRef } from 'react';
 
-export const Service = () => {
+// Use forwardRef to forward the ref to the div
+export const Service = forwardRef((props, ref) => {
   return (
-    <div className='max-w-full w-full h-screen flex justify-center text-center mx-auto flex-col'>
-        <h1 className='text-5xl m-20'>
-            How you Feeling today
-        </h1>
-        <div>
-            <input className="text-3xl font-extralight border min-w-[800px] h-20 text-center rounded-full hover:shadow-2xl" type="text" placeholder='HOW YOU FEELING ' />
-        </div>
-        <div className='mt-20'>
-            <button className='border rounded-full p-5 hover:shadow-2xl'>FIND SONG FOR ME</button>
-        </div>
-
+    <div ref={ref} className="max-w-full w-full h-screen flex justify-center text-center mx-auto flex-col">
+      <h1 className="text-5xl m-20">
+        How you Feeling today
+      </h1>
+      <div>
+        <input
+          className="text-3xl font-extralight border min-w-[800px] h-20 text-center rounded-full hover:shadow-2xl"
+          type="text"
+          placeholder="HOW YOU FEELING "
+        />
+      </div>
+      <div className="mt-20">
+        <button className="border rounded-full p-5 hover:shadow-2xl">FIND SONG FOR ME</button>
+      </div>
     </div>
-  )
-}
+  );
+});
